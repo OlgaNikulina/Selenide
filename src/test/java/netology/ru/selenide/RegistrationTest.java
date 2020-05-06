089c64af60d8fc4c50af7de609a1c4ff1ac010d9
OlgaNikulina <nikulinaov@inbox.ru>
2020-05-06 13:48:34 +0500

    notification title added

--- src/test/java/netology/ru/selenide/RegistrationTest.java
+++ src/test/java/netology/ru/selenide/RegistrationTest.java
@@ -9,14 +9,13 @@ import java.security.Key;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationTest {
 
-    private LocalDate date = LocalDate.of(2020, 5, 02);
+    private LocalDate date = LocalDate.now();
     private LocalDate localDate;
 
     @Test
@@ -24,17 +23,17 @@ public class RegistrationTest {
         open("http://localhost:7777");
         $("[placeholder= 'Город']").setValue("Санкт-Петербург");
         $("span.menu-item__control").click();
-        $("[type='date']").sendKeys(Keys.CONTROL +"a");
-        $("[type='date']").sendKeys(Keys.DELETE);
+        $("[placeholder='Дата встречи']").sendKeys(Keys.CONTROL +"a");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.DELETE);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         localDate = date.plusDays(3);
         String futureDate = formatter.format(localDate);
-        $("[type='date']").setValue(futureDate);
+        $("[placeholder='Дата встречи']").setValue(futureDate);
         $("[name='name']").setValue("Василий Шукшин");
         $("[name='phone']").setValue("+79123456789");
         $("span[class='checkbox__text']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно! Встреча забронирована на 03.05.2020")).waitUntil(visible, 15000);
+        $("div.notification__title").waitUntil(visible, 15000);
     }
 
     public LocalDate getDate() {
